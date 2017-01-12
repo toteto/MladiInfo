4260f39dd2b698acdeaaa67cd94d50d3953c1678
toteto <toteto@users.noreply.github.com>
2017-01-12 18:08:30 +0100

    Sample testing call to the api

--- app/src/main/java/mk/ams/mladi/mladiinfo/MainActivity.kt
+++ app/src/main/java/mk/ams/mladi/mladiinfo/MainActivity.kt
@@ -1,12 +1,35 @@
 package mk.ams.mladi.mladiinfo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import mk.ams.mladi.mladiinfo.DataModels.Training
+import mk.ams.mladi.mladiinfo.DataProviders.MladiInfoApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    MladiInfoApiClient.client.getTraining().enqueue(object : Callback<String> {
+      override fun onResponse(call: Call<String>?, response: Response<String>?) {
+        val rawData = response?.body().toString()
+        val trainings: List<Training> = Gson().fromJson(rawData, object : TypeToken<List<Training>>() {}.type)
+        Log.d("asd", trainings.toString())
+      }
+
+      override fun onFailure(call: Call<String>?, t: Throwable?) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+      }
+    })
   }
 }
