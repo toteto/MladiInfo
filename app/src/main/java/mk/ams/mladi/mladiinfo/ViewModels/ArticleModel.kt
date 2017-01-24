cd53ed22f3cbfefda3b5cbd48176fd502afbe50f
= <Antonio.Ivanovski@netcetera.com>
2017-01-24 20:48:12 +0100

    Update ArticleModel equals for diffing purposes

--- app/src/main/java/mk/ams/mladi/mladiinfo/ViewModels/ArticleModel.kt
+++ app/src/main/java/mk/ams/mladi/mladiinfo/ViewModels/ArticleModel.kt
@@ -5,12 +5,26 @@ import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.EpoxyModelWithHolder
 import kotlinx.android.synthetic.main.article_item.view.*
+import mk.ams.mladi.mladiinfo.DataModels.Scholarship
 import mk.ams.mladi.mladiinfo.DataModels.Training
+import mk.ams.mladi.mladiinfo.DataModels.Work
 import mk.ams.mladi.mladiinfo.R
 
 class ArticleModel(val title: String, val description: String, val date: String, val source: String)
   : EpoxyModelWithHolder<ArticleModel.ArticleItemVH>() {
-  constructor(training: Training) : this(training.title, training.description, training.crawlDate, training.siteName)
+  var id = hashCode().toLong()
+
+  constructor(training: Training) : this(training.title, training.description, training.crawlDate, training.siteName) {
+    id(training.id.toLong())
+  }
+
+  constructor(scholarship: Scholarship) : this(scholarship.title, scholarship.description, scholarship.crawlDate, scholarship.siteName) {
+    id(scholarship.id.toLong())
+  }
+
+  constructor(work: Work) : this(work.id, work.description, work.inserted, work.websiteID) {
+    id(work.id.toLong())
+  }
 
   override fun createNewHolder() = ArticleItemVH()
 
@@ -23,7 +37,6 @@ class ArticleModel(val title: String, val description: String, val date: String,
     holder.itemView.tvArticleSource.text = source
   }
 
-
   class ArticleItemVH : EpoxyHolder() {
     lateinit var itemView: View
     override fun bindView(itemView: View) {
@@ -31,9 +44,36 @@ class ArticleModel(val title: String, val description: String, val date: String,
     }
   }
 
-  override fun id(): Long = 31 * title.hashCode().toLong() + description.hashCode().toLong()
+  override fun id(): Long {
+    return id
+  }
 
   override fun id(id: Long): EpoxyModel<ArticleItemVH> {
+    this.id = id
     return this
   }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other?.javaClass != javaClass) return false
+    if (!super.equals(other)) return false
+
+    other as ArticleModel
+
+    if (title != other.title) return false
+    if (date != other.date) return false
+    if (id != other.id) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = super.hashCode()
+    result = 31 * result + title.hashCode()
+    result = 31 * result + date.hashCode()
+    result = 31 * result + id.hashCode()
+    return result
+  }
+
+
 }
